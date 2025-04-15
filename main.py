ab694a26eaa0904bbcf6242ff12488db93566293
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-04-15 21:20:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,133 +5,109 @@ from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
 
-def request_to_api_hh(lang):
+VACANCIES_PER_PAGE_ON_HH = 100
+MAX_PAGES_ON_HH = 20
+ID_MOSCOW_AREA_ON_HH = 1
+VACANCIES_PER_PAGE_ON_SJ = 100
+KEY_OF_CATALOG_SJ = 48
+CITY_NAME_ON_SJ = 'Moscow'
+
+
+def get_request_to_api_hh(lang):
     page = 0
-    results = []
-    try:
-        while page < 19:
-            payload = {
-                'area': '1',
-                'text': f'Программист {lang}',
-                'currency': 'RUR',
-                'page': page,
-                'per_page': 99
-            }
-            url = 'https://api.hh.ru/vacancies'
-            response = requests.get(url, params=payload)
-            response.raise_for_status()
-            data = response.json()
-            results.append(data)
-            page += 1
-            return {
-                lang: results
-            }
-    except requests.exceptions.HTTPError:
-        return {
-            lang: results
+    vacancies_filtered_by_lang_on_hh = []
+
+    while page < MAX_PAGES_ON_HH:
+        payload = {
+            'area': ID_MOSCOW_AREA_ON_HH,
+            'text': f'Программист {lang}',
+            'currency': 'RUR',
+            'page': page,
+            'per_page': VACANCIES_PER_PAGE_ON_HH
         }
+        url = 'https://api.hh.ru/vacancies'
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+        request_result_json = response.json()
+        vacancies_filtered_by_lang_on_hh.append(request_result_json)
+        page += 1
+
+    return {
+        lang: vacancies_filtered_by_lang_on_hh
+    }
 
 
-def predict_rub_salary_hh(lang):
-    vacancies_list = request_to_api_hh(lang)[lang][0]['items']
+def get_predict_rub_salary_hh(lang):
+    vacancies = get_request_to_api_hh(lang)[lang][0]['items']
 
-    list_of_salary = []
-    for list_all_vacancies in vacancies_list:
-        salary = list_all_vacancies.get('salary')
-        list_of_salary.append(salary)
+    salaries = []
+    for salary_of_vacancies in vacancies:
+        salary = salary_of_vacancies.get('salary')
+        salaries.append(salary)
 
-    predicts_salary = []
-    for predict in list_of_salary:
-        if predict is not None:
+    salary_expectations = []
+    for predict in salaries:
+        if predict:
 
-            if predict['from'] is not None:
-                predicts_salary.append(predict['from']*1.2)
+            if predict['to'] and predict['from']:
+                salary_expectations.append((predict['to'] + predict['from'])/2)
 
-            elif predict['to'] is not None:
-                predicts_salary.append(predict['to']*0.8)
+            elif predict['from']:
+                salary_expectations.append(predict['from']*1.2)
 
-    return predicts_salary
+            elif predict['to']:
+                salary_expectations.append(predict['to']*0.8)
+
+    return [lang, len(vacancies), len(salary_expectations), statistics.mean(salary_expectations)]
 
 
-def request_to_api_super_job(lang):
+def get_request_to_api_super_job(lang):
     url = '	https://api.superjob.ru/2.0/vacancies/'
     headers = {
         'X-Api-App-Id': os.getenv('superjob_token'),
     }
-    page = 0
-    results = []
-    while page < 19:
-        payload = {
-            "keyword": f"Программист {lang}",
-            'town': 'Москва',
-            'page':page,
-            'count':99
-        }
-        response = requests.get(url, headers=headers, params=payload)
-        response.raise_for_status()
-        data = response.json()
-        results.append(data)
-        page += 1
+    vacancies_filtered_by_lang_on_sj = []
+    payload = {
+        "keyword": f"Программист {lang}",
+        'town': CITY_NAME_ON_SJ,
+    }
+    response = requests.get(url, headers=headers, params=payload)
+    response.raise_for_status()
+    request_result_json = response.json()
+    vacancies_filtered_by_lang_on_sj.append(request_result_json)
+
     return {
-        lang: results
+        lang: vacancies_filtered_by_lang_on_sj
     }
 
 
-def predict_rub_salary_for_superJob(lang):
-    vacancies_list_sj = request_to_api_super_job(lang)[lang][0]['objects']
+def get_predict_rub_salary_for_superJob(lang):
+    vacancies = get_request_to_api_super_job(lang)[lang][0]['objects']
 
-    list_of_salary = []
-    for list_all_vacancies in vacancies_list_sj:
+    salary_expectations = []
+    for vacancy in vacancies:
 
-        payment_from = list_all_vacancies.get('payment_from')
-        payment_to = list_all_vacancies.get('payment_to')
+        payment_from = vacancy.get('payment_from')
+        payment_to = vacancy.get('payment_to')
 
-        if payment_from != 0:
-            list_of_salary.append(payment_from*1.2)
+        if payment_from and payment_to:
+            salary_expectations.append((payment_from+payment_to)/2)
 
-        elif payment_to != 0:
-            list_of_salary.append(payment_to*0.8)
+        elif payment_from:
+            salary_expectations.append(payment_from*1.2)
 
-    return list_of_salary
+        elif payment_to:
+            salary_expectations.append(payment_to*0.8)
+
+    return [lang, len(vacancies), len(salary_expectations), statistics.mean(salary_expectations)]
 
 
-def get_stats_of_salary_from_sj(langs, table_name):
-    table_data = [
-        ['Язык программирования',
-         'Вакансий найдено',
-         'Вакансий обработано',
-         'Средняя зарплата'],
-    ]
+def get_stats_of_salary(langs, salary_expectations, table_name, table_data):
+    current_table_data = [row[:] for row in table_data]
     for lang in langs:
-        founds_vacancies_from_sj = request_to_api_super_job(lang)[lang][0]['objects']
-        table_data.extend([
-            [
-                lang, len(founds_vacancies_from_sj),
-                len(predict_rub_salary_for_superJob(lang)),
-                statistics.mean(predict_rub_salary_for_superJob(lang))
-             ],
-        ])
-    table = AsciiTable(table_data, title=table_name)
-    return table.table
-
-
-def get_stats_of_salary_from_hh(langs, table_name):
-    table_data = [
-        ['Язык программирования',
-         'Вакансий найдено',
-         'Вакансий обработано',
-         'Средняя зарплата'],
-    ]
-    for lang in langs:
-        founds_vacancies_from_hh = request_to_api_hh(lang)[lang][0]['found']
-        table_data.extend([
-            [
-                            lang, founds_vacancies_from_hh,
-                            len(predict_rub_salary_hh(lang)),
-                            statistics.mean(predict_rub_salary_hh(lang))
-            ]
-        ])
-    table = AsciiTable(table_data, title=table_name)
+        current_table_data.extend([salary_expectations(lang)])
+    table = AsciiTable(current_table_data, title=table_name)
+    print(table.table)
     return table.table
 
 
@@ -142,8 +118,14 @@ def main():
         'Java',
         'Javascript'
     ]
-    print(get_stats_of_salary_from_hh(langs, 'HH Moscow'))
-    print(get_stats_of_salary_from_sj(langs, 'SuperJob Moscow'))
+    table_data = [
+        ['Язык программирования',
+         'Вакансий найдено',
+         'Вакансий обработано',
+         'Средняя зарплата'],
+    ]
+    get_stats_of_salary(langs, get_predict_rub_salary_for_superJob, 'SJ MOSCOW', table_data)
+    get_stats_of_salary(langs, get_predict_rub_salary_hh, 'HH MOSCOW', table_data)
 
 
 if __name__ == "__main__":
